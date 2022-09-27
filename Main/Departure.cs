a75bb5f488232f3508024d53342d796c468bed08
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2022-09-27 23:02:42 +0200

    Add string limit

--- Main/Departure.cs
+++ Main/Departure.cs
@@ -10,19 +10,21 @@ namespace NextBuses
 		[JsonPropertyName("DepartureBoard")]
 		public DepartureBoard Board { get; set; }
 		// Display the next 5 departures
-		public string display(HashSet<string> line, HashSet<string> direction)
+		public string display(HashSet<string> line, HashSet<string> direction, int limit = 10)
 		{
 			if (Board.Departures.Count() == 0)
             {
 				throw new InvalidDataException("Error no departure at this stop.");
             }
-			string display = $"{Board.Departures.First().Stop} ";
+			Departure first = Board.Departures.First();
+			int stringLimit = first.Stop.Count() > limit ? limit : first.Stop.Count();
+			string display = $"{first.Stop.Substring(0, stringLimit)} ";
 			var next = Board.Departures.Where(d => line.Contains(d.Line) && direction.Contains(d.Direction)).Take(5);
 			if (next.Count() == 0)
             {
 				return display + "No departure for line or direction.";
             }
-			Departure first = next.First();
+			first = next.First();
 			
 			foreach (Departure departure in next)
 			{

adc56bc21811eac2b053777a7f2a02acd2fd112f
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2022-09-27 22:06:03 +0200

    add csproj file

---
+++ Main/Body.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace NextBuses
+{
+    public class Body
+    {
+        [JsonPropertyName("requestedStops")]
+        public List<RequestedStop> RequestedStops { get; set; }
+    }
+    public class RequestedStop
+    {
+        [JsonPropertyName("id")]
+        public string StopID { get; set; }
+        [JsonPropertyName("Lines")]
+        public List<string> Lines { get; set; }
+        [JsonPropertyName("Directions")]
+        public List<string> Directions { get; set; }
+    }
+}
+
